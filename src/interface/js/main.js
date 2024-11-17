787d9021ae738083d09a277715f92209ca63aa65
João Pacheco <joao.pacheco@visie.com.br>
2024-11-17 17:13:56 -0300

    Initial commit

---
+++ src/interface/js/main.js
@@ -0,0 +1,31 @@
+
+const positionsUsed = [];
+
+const positions = [
+    // Posicionamento Superior
+    { top: '2%', left: '10%' },
+    { top: '2%', left: '22%' },
+    { top: '2%', left: '34%' },
+    { top: '2%', left: '46%' },
+    { top: '2%', left: '58%' },
+    { top: '2%', left: '70%' },
+    { top: '2%', left: '82%' },
+
+    // Posicionamento Esquerdo
+    { top: '26%', left: '10%' },
+    { top: '51%', left: '10%' },
+
+    // Posicionamento Direito
+    { top: '26%', left: '82%' },
+    { top: '51%', left: '82%' },
+
+    // Posicionamento Inferior
+    { top: '75%', left: '10%' },
+    { top: '75%', left: '22%' },
+    { top: '75%', left: '34%' },
+    { top: '75%', left: '46%' },
+    { top: '75%', left: '58%' },
+    { top: '75%', left: '70%' },
+    { top: '75%', left: '82%' }
+];
+
